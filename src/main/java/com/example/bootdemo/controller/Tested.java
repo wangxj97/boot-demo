ad08e290a6ad2cfd8345adcd8de7095d4009f837
wangxj97 <lywangxingjun@163.com>
2020-04-04 19:50:20 +0800

    远程仓库测试

--- src/main/java/com/example/bootdemo/controller/Tested.java
+++ src/main/java/com/example/bootdemo/controller/Tested.java
@@ -1,4 +1,10 @@
 package com.example.bootdemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tested {
+    public void share(){
+        List<String> list = new ArrayList<>();
+    }
 }
